491586f47206837c7a49dfeae7ea4e1fb9a43e21
Dmitry Malkov <nickfailure@gmail.com>
2025-03-16 20:39:56 +0300

    Makes the `LogsAPI` constructor more generic

--- src/logs_api/logs_api.py
+++ src/logs_api/logs_api.py
@@ -12,11 +12,11 @@ class LogsAPI():
 
     def __init__(
             self,
-            fields: list[str],
             auth_token: str,
             counter_id: int,
-            start_date: str,
-            end_date: str,
+            fields: list[str] | None = None,
+            start_date: str | None = None,
+            end_date: str | None = None,
             source: str | list[str] = ['visits', 'hits'],
             params: dict[str, Any] = {}
     ):
@@ -24,12 +24,19 @@ class LogsAPI():
             access_token=auth_token,
             default_url_params={'counterId': counter_id}
         )
+
         self.params = {
             'source': source,
-            'fields': fields,
-            'date1': start_date,
-            'date2': end_date
-        } | params
+        }
+        
+        if fields and len(fields) > 0:
+            self.params.update({'fields': fields})
+        if start_date:
+            self.params.update({'date1': start_date})
+        if end_date:
+            self.params.update({'date2': end_date})
+        
+        self.params.update(params)
 
     def create_report(self, params: dict[str, Any] = {}) -> int:
         result = self.client.create().post(params=self.params | params)
@@ -44,8 +51,11 @@ class LogsAPI():
         except Exception as e:
             return ReportCheckResult(False, e)
 
-    def get_report_info(self, request_id: int) -> dict:
+    def get_report_info(self, request_id: int):
         return self.client.info(requestId=request_id).get()
+    
+    def get_all_reports_info(self):
+        return self.client.allinfo().get()
 
     def is_report_ready(self, request_id: int) -> bool:
         info = self.get_report_info(request_id)
